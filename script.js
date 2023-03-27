6798beaead95ccdc7b555150aefef17eebe77f8e
wangxinyi <wangxinyi@tsingj.com>
2023-03-27 23:44:11 +0800

    added score

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.querySelector('.dino');
 const obstacle = document.querySelector('.obstacle');
+const scoreElement = document.getElementById('scoreValue');
 let isJumping = false;
+let score = 0;
 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Space' && !isJumping) {
@@ -36,19 +38,10 @@ function jump() {
     }, 20);
 }
 
-setInterval(() => {
-    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('bottom'));
-    const obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
-    const obstacleLeft = window.innerWidth - obstacleRight - obstacle.clientWidth;
-
-    if (obstacleLeft >= 0 && obstacleLeft < 40 && dinoTop <= 40) {
-        alert('Game Over! Reload the page to play again.');
-        gameOver()
-    }
-}, 50);
-
 function gameOver() {
     obstacle.style.animation = 'none';
+    score = 0;
+    scoreElement.textContent = score;
     setTimeout(() => {
         startObstacle();
     }, 1000);
@@ -57,3 +50,21 @@ function gameOver() {
 function startObstacle() {
     obstacle.style.animation = 'moveObstacle 2s infinite linear';
 }
+
+function updateScore() {
+    score++;
+    scoreElement.textContent = score;
+}
+
+setInterval(() => {
+    const dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('bottom'));
+    const obstacleRight = parseInt(window.getComputedStyle(obstacle).getPropertyValue('right'));
+    const obstacleLeft = window.innerWidth - obstacleRight - obstacle.clientWidth;
+
+    if (obstacleLeft >= 0 && obstacleLeft < 40 && dinoTop <= 40) {
+        alert('Game Over! Reload the page to play again.');
+        gameOver()
+    } else {
+        updateScore();
+    }
+}, 50);
